41b8fb097eef56f356df51ae352fbb1fd7d82b0b
m-haris-n <m.haris.nabeel@gmail.com>
2025-04-24 01:03:52 +0500

    added callback for email

---
+++ src/app/api/v1/auth/callback/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server'
+// import { redirect } from 'next/navigation'
+
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url)
+    const type = searchParams.get('type')
+
+    if (type === 'confirm-email') {
+      // Redirect to mobile app home page using custom URL scheme
+      return NextResponse.redirect('bankrai://')
+    }
+
+    return NextResponse.json({ type }, { status: 200 })
+  } catch (error) {
+    console.error('Callback error:', error)
+    return NextResponse.json(
+      { error: 'Something went wrong' },
+      { status: 500 }
+    )
+  }
+}
